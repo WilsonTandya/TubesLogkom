67d105575553347cef8e8d1d386111abfbbed5e9
thivh <nizamilutvie@gmail.com>
2020-11-22 21:38:38 +0700

    Revert "Merge branch 'main' of https://github.com/WilsonTandya/TubesLogkom into main"
    This reverts commit 9200c4dba798fb201544075fa8c416062c866179, reversing
    changes made to e4728463bec5ee23d6b6ea4b0a7487d0299f3aec.

--- map.pl
+++ map.pl
@@ -1,42 +1,46 @@
+:- dynamic(lebarpeta/1).
+:- dynamic(tinggipeta/1).
 :- dynamic(quest/2).
 :- dynamic(dungeon/2).
 :- dynamic(store/2).
 :- dynamic(player/2).
-:- dynamic(obstacle/2).
+:- dynamic(isMulai/0).
 
 lebarpeta(15).
 tinggipeta(15).
 
-/* pagaratas Y = 0 */
-pagaratas(_,Y) :-
+mulai :- asserta(isMulai),
+	asserta(player(3,2)),
+	asserta(quest(5,10)),
+	asserta(dungeon(13,13)),
+	asserta(store(12,4)),
+	!.
+
+borderatas(_,Y) :-
 	Y =:= 0,!.
-/* pagarbawah tinggipeta+1 */
-pagarbawah(_,Y) :-
+
+borderbawah(_,Y) :-
 	tinggipeta(T),
 	Ymax is T+1,
 	Y =:= Ymax,!.
-/* pagarkiri X = 0 */
-pagarkiri(X,_) :-
+
+borderkiri(X,_) :-
 	X =:= 0,!.
-/* pagarkiri lebarpeta+1 */
-pagarkanan(X,_) :-
+
+borderkanan(X,_) :-
 	lebarpeta(L),
 	Xmax is L+1,
 	X =:= Xmax,!.
 
-/* print peta untuk pager */
 printpeta(X,Y) :-
-	pagaratas(X,Y), !, write('#').
+	borderatas(X,Y), !, write('#').
 printpeta(X,Y) :-
-	pagarbawah(X,Y), !, write('#').
+	borderbawah(X,Y), !, write('#').
 printpeta(X,Y) :-
-	pagarkiri(X,Y), !, write('#').
+	borderkiri(X,Y), !, write('#').
 printpeta(X,Y) :-
-	pagarkanan(X,Y), !, write('#').
-printpeta(X,Y) :-
-	obstacle(X,Y), !, write('#').
+	borderkanan(X,Y), !, write('#').
 
-/* print peta untuk objek */
 printpeta(X,Y) :-
 	player(X,Y), !, write('P').
 printpeta(X,Y) :-
@@ -45,31 +49,13 @@ printpeta(X,Y) :-
 	dungeon(X,Y), !, write('D').
 printpeta(X,Y) :-
 	store(X,Y), !, write('S').
-/*Print sisa peta*/
 printpeta(_,_) :-
 	write('-').
 
-map :-
-  \+isPlay,
-  write('You must start the game first!'), nl, !.
-
 map:-
-	asserta(quest(5,10)),
-	asserta(dungeon(13,13)),
-	asserta(store(12,4)),
-	/* Obstacle atas kiri */
-	asserta(obstacle(6,2)),asserta(obstacle(6,3)),asserta(obstacle(6,4)),
-	asserta(obstacle(6,5)),asserta(obstacle(5,5)),asserta(obstacle(4,5)),
-	asserta(obstacle(3,5)),
-	/* Obstacle Quest */
-	asserta(obstacle(4,9)),asserta(obstacle(5,9)),asserta(obstacle(6,9)),
-	asserta(obstacle(7,9)),asserta(obstacle(7,10)),asserta(obstacle(7,11)),
-	asserta(obstacle(3,9)),asserta(obstacle(3,10)),asserta(obstacle(3,11)),
-	/* Obstacle Dungeon */
-	asserta(obstacle(11,14)),asserta(obstacle(11,13)),
-	asserta(obstacle(12,14)),asserta(obstacle(13,14)),asserta(obstacle(14,14)),
-	asserta(obstacle(11,12)),asserta(obstacle(12,12)),asserta(obstacle(13,12)),
-	asserta(obstacle(14,12)),asserta(obstacle(15,12)),
+	\+isMulai,
+	write('jalankan command (mulai.) terlebih dahulu'), nl, !.
+map:-
 	tinggipeta(T),
 	lebarpeta(L),
 	X is 0,
@@ -78,8 +64,11 @@ map:-
 	Ymax is T+1,
 	forall(between(Y, Ymax, J), (
 		forall(between(X, Xmax, I), (
-			printpeta(I,J))),nl)),
-	write(' Legenda:'), nl,
+			printpeta(I,J)
+		)),
+		nl
+	)),
+	write(' Legends:'), nl,
 	write('    P = Player'), nl,
 	write('    Q = Quest'), nl,
 	write('    D = Dungeon'), nl,
--- walk.pl
+++ walk.pl
@@ -1,83 +1,11 @@
-w :-
-  \+isPlay,
-  write('You must start the game first!'), nl, !.
+w :- write('you move forward').
+a :- write('you move west').
+s :- write('you move backward').
+d :- write('you move east').
 
-w :-
-  isPlay, player(_, Y),
-  Y =:= 1,
-  write('You cant move forward! there is an obstacle in front of you'), nl, !.
-
-w :-
-  isPlay, player(X, Y), obstacle(X, Z),
-  Y-1 =:= Z,
-  write('You cant move forward! there is an obstacle in front of you'), nl, !.
-
-w :-  
-  isPlay, retract(player(X,Y)),
-	NewY is Y - 1,
-	write('You move forward'), nl,
-	asserta(player(X, NewY)),
-	!.
-
-a :-
-  \+isPlay,
-  write('You must start the game first!'), nl, !.
-
-a :-
-  isPlay, player(X, _),
-  X =:= 1,
-  write('You cant move west! there is an obstacle on your left'), nl, !.
-
-a :-
-  isPlay, player(X, Y), obstacle(Z, Y),
-  X-1 =:= Z,
-  write('You cant move west! there is an obstacle on your left'), nl, !.
-
-a :- 
-  isPlay, retract(player(X,Y)),
-	NewX is X - 1,
-	write('You move west'), nl,
-	asserta(player(NewX, Y)), 
-	!.
-
-s :-
-  \+isPlay,
-  write('You must start the game first!'), nl, !.
-
-s :-
-  isPlay, player(_, Y),
-  Y =:= 15,
-  write('You cant move backward! there is an obstacle behind you'), nl, !.
-
-s :-
-  isPlay, player(X, Y), obstacle(X, Z),
-  Y+1 =:= Z,
-  write('You cant move backward! there is an obstacle behind you'), nl, !.
-
-s :- 
-  isPlay, retract(player(X,Y)),
-	NewY is Y + 1,
-	write('You move backward'), nl,
-	asserta(player(X, NewY)),
-	!.
-
-d :-
-  \+isPlay,
-  write('You must start the game first!'), nl, !.
-
-d :-
-  isPlay, player(X, _),
-  X =:= 15,
-  write('You cant move east! there is an obstacle on your right'), nl, !.
-
-d :-
-  isPlay, player(X, Y), obstacle(Z, Y),
-  X+1 =:= Z,
-  write('You cant move east! there is an obstacle on your right'), nl, !.
-
-d :- 
-  isPlay, retract(player(X,Y)),
-	NewX is X + 1,
-	write('You move east'), nl,
-	asserta(player(NewX, Y)), 
-	!.
+/* 
+Todo
+1. move beneran di map
+2. cek tembok
+3. random ketemu musuh(apakah disini?)
+*/
