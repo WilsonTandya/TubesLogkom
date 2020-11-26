1a307566c78c535d2f67c17b2af20dd59de9fcf4
arsa dg <arsaadg891@gmail.com>
2020-11-26 19:43:17 +0700

    shop harusnya fix

--- shop.pl
+++ shop.pl
@@ -15,16 +15,17 @@ shop :-
   ; !, write('You must go to the shop first!'), nl).
 
 gacha :-
-  isPlay, isShop, inventCounter(Y), 
-  ( (Y+1) < 101 -> 
-  random(1, 101, X), write('You got a '),
+  isPlay, isShop, inventCounter(Y), currGold(Gold),
+  ( Gold > 199 -> !, ( (Y+1) < 101 -> 
+  random(1, 101, X), write('You got a '), retract(currGold(Gold)), NewGold is (Gold-200), asserta(currGold(NewGold)),
   (X < 31 -> !,item(55, B, C, _), addToInvent(55), write(C), write(' '), write(B), nl
   ; 30 < X, X < 61 -> random(10, 19, A),!, item(A, B, C, _), addToInvent(A), write(C), write(' '), write(B), nl
   ; 60 < X, X < 81 -> random(19, 28, A),!, item(A, B, C, _), addToInvent(A), write(C), write(' '), write(B), nl
   ; 80 < X, X < 94 -> random(28, 37, A),!, item(A, B, C, _), addToInvent(A), write(C), write(' '), write(B), nl
   ; 93 < X, X < 99 -> random(37, 46, A),!, item(A, B, C, _), addToInvent(A), write(C), write(' '), write(B), nl
   ; 98 < X, X < 101 -> random(46, 55, A),!, item(A, B, C, _), addToInvent(A), write(C), write(' '), write(B), nl)
-  ; !, write('Your inventory is full'), nl).
+  ; !, write('Your inventory is full'), nl)
+  ; !, write('You cant afford a gacha (200 gold)')).
 
 gacha :-
   \+isPlay,
@@ -35,9 +36,12 @@ gacha :-
   write('You must go to the shop first!'), nl, !.
 
 potion :-
-  isShop, item(55, B, C, _), inventCounter(Y),
-  ( (Y+1) < 101 -> !, write('You got a '), write(C), write(' '), write(B), addToInvent(55), nl
-  ; !, write('Your inventory is full'), nl).
+  isShop, item(55, B, C, _), inventCounter(Y), currGold(Gold),
+  (Gold > 99 -> !, 
+  ( (Y+1) < 101 -> !, retract(currGold(Gold)), NewGold is (Gold-100), asserta(currGold(NewGold)),
+  write('You got a '), write(C), write(' '), write(B), addToInvent(55), nl
+  ; !, write('Your inventory is full'), nl)
+  ; !, write('You cant afford a potion (100 gold)')).
 
 potion :-
   \+isShop,
